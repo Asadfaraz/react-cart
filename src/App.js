86b24cb9a84f1b1869bc4aa1067d119186922d74
Asad Faraz <asadfaraz44@gmail.com>
2021-05-09 01:02:25 +0500

    basic setup context api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import "./App.css";
 
+import { useGlobalContext } from "./context";
+
+// components
+import Navbar from "./Navbar";
+import CartContainer from "./CartContainer";
+
 function App() {
+  // if (loading) {
+  //   return (
+  //     <div>
+  //       <h1>Loading...</h1>
+  //     </div>
+  //   );
+  // }
+
+  // Return
   return (
-    <div className="App">
-      <h3>Cart</h3>
-    </div>
+    <main>
+      <Navbar />
+      <CartContainer />
+    </main>
   );
 }
 
