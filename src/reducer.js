6af9d5d0768981fe1c9607aa4551aadeaca9a3f6
Asad Faraz <asadfaraz44@gmail.com>
2021-05-11 13:57:36 +0500

    single item remove

--- src/reducer.js
+++ src/reducer.js
@@ -2,6 +2,15 @@ const reducer = (state, action) => {
   if (action.type == "CLEAR_CART") {
     return { ...state, cart: [] };
   }
+
+  if (action.type == "REMOVE") {
+    const newCartItems = state.cart.filter(
+      (cartItem) => cartItem.id != action.payload
+    );
+
+    return { ...state, cart: newCartItems };
+  }
+
   return state;
 };
 
